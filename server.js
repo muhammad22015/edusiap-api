03e4ab06e0cf8e2fe1e9df9fd7b3900fc5f1f248
muhammad22015 <muhammad22015@mail.unpad.ac.id>
2025-06-10 22:47:52 +0700

    Pembaharuan server.js

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const router = require('./routes')
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://edusiap-api-498867854322.asia-southeast2.run.app'
+    ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,9 +16,9 @@ app.get('/', (req,res) => {
     res.send("Welcome to EduSiap API");
 })
 
-app.use('/', router);   
+app.use('/', router);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Server running on ${BASE_URL}:${PORT}`)
 })
